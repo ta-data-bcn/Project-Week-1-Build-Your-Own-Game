316fb6780ea58d60d685d0b1821a0c0c270bb94c
Carleto04 <rosell.carles@gmail.com>
2020-06-25 15:40:02 +0200

    commiting V1 betagame

--- your-project/library_funct/letter_choice.py
+++ your-project/library_funct/letter_choice.py
@@ -1,22 +1,8 @@
-#defining letter choice
 def letter_choice():
-
     alphabet = list(map(chr, range(97, 123)))
     letter_choice = []
 
     import random
-    choosing_player = random.choice(players)
-
-    if choosing_player in cpu_teams:
-        print(choosing_player + " choose!")
-        letter = random.choice(alphabet)
-        letter_choice.append(letter)
-        print("This rounds letter is" + letter)
-
-    else:
-        letter_player = input("You choose! What letter will be? ")
-        print(letter_player)
-        if letter_choice.lower() in alphabet:
-            letter_choice.append(letter_player)
-        else:
-            print(input("You choose! What letter will be? "))
+    letter = random.choice(alphabet)
+    letter_choice.append(letter)
+    print("This rounds letter is" + letter)
