5085318eb4cb4cee9f2be4948949a2bd297c2781
Carleto04 <rosell.carles@gmail.com>
2020-06-25 13:32:48 +0200

    checkpoint

---
+++ your-project/library_funct/user_name.py
@@ -0,0 +1,4 @@
+##defining name of user team
+def user_name():
+    user_team = str(input("What is your teams name? "))
+    print(user_team + " will play against " + str(num_cpu_teams) + " cpu teams!")
