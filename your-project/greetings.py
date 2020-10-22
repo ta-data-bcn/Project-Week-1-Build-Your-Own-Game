83ad4f92d375b32005284d1cbeeac4cb21d220ce
Oscar Tomas <ooscar13@gmail.com>
2020-10-22 20:02:59 +0200

    proyecto definitivo

---
+++ your-project/greetings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def greetings(district_list):
+    
+    wannaplay=True
+    
+    print("Hi Ironhackers, did you survive the first week? Cool!!! Honestly I didn't even believe in myself. But now, let's play Risk game to enjoy.")
+    print("We'll start with 2 districts: ", district_list)
+    time.sleep(8)
+
